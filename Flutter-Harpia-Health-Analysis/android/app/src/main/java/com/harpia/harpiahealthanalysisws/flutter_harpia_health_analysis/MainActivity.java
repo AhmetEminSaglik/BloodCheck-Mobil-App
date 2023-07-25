4cbd9ace8e0afbaedd0411cca4b9280b55157ad1
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2023-07-25 18:14:24 +0300

    Flutter initial

---
+++ Flutter-Harpia-Health-Analysis/android/app/src/main/java/com/harpia/harpiahealthanalysisws/flutter_harpia_health_analysis/MainActivity.java
@@ -0,0 +1,6 @@
+package com.harpia.harpiahealthanalysisws.flutter_harpia_health_analysis;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
