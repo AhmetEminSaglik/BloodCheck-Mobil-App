5524197863fb5553e796d5c22228ea1481692424
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2023-07-26 15:16:21 +0300

    Product color is added to use same colors for whole app

---
+++ Flutter-Harpia-Health-Analysis/lib/util/ProductColor.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ProductColor {
+/*  static bool _darkThemeActivated = false;
+
+  static void updateTheme({required bool darkThemeActivated}) {
+    _darkThemeActivated = darkThemeActivated;
+  }
+
+  static get backgroundColor =>
+      {_darkThemeActivated ? _productWhite : _productBlack};
+
+  static get textColor =>
+      {_darkThemeActivated ? _productWhite : _productBlack};*/
+
+
+  static const _darkBlue = Color(0xFF18344a);
+  static const _black = Color(0xFF29293A);
+  static const _white = Colors.white;
+  static const _green = Colors.green;
+
+  static get darkBlue => _darkBlue;
+
+  static get black => _black;
+
+  static get white => _white;
+
+  static get green => _green;
+}
