67938c09be4f6f4ce90f6c3cee8629effd59f17e
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2023-08-04 06:57:00 +0300

    user password is encrypted Fixed #5

--- Harpia-Health-Analysis-WS/src/main/java/com/harpia/HarpiaHealthAnalysisWS/business/concretes/user/UserManager.java
+++ Harpia-Health-Analysis-WS/src/main/java/com/harpia/HarpiaHealthAnalysisWS/business/concretes/user/UserManager.java
@@ -4,23 +4,32 @@ import com.harpia.HarpiaHealthAnalysisWS.business.abstracts.user.UserService;
 import com.harpia.HarpiaHealthAnalysisWS.dataaccess.user.UserRepository;
 import com.harpia.HarpiaHealthAnalysisWS.model.users.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-
 public class UserManager implements UserService {
+
+
     @Autowired
     UserRepository userRepository;
 
+    PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     @Override
-    public User save(User u) {
-        return userRepository.save(u);
+    public User save(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
     }
 
     @Override
     public List<User> saveAll(List<User> list) {
+        list.forEach(user -> {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+        });
         return userRepository.saveAll(list);
     }
 
