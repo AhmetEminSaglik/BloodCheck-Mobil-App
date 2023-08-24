ae235da6e0c6e2c562a19eab796afa47ea45e7c2
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2023-08-24 18:12:26 +0300

    Style Information is added to change the notification text color as send emmbeded html format

--- Flutter-Harpia-Health-Analysis/lib/util/CustomNotification.dart
+++ Flutter-Harpia-Health-Analysis/lib/util/CustomNotification.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_harpia_health_analysis/util/ProductColor.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class CustomNotificationUtil {
@@ -22,16 +23,23 @@ class CustomNotificationUtil {
   }
 
   static Future<void> showNotification(String title, String msg) async {
-    var androidNotificationDetail = const AndroidNotificationDetails(
+    var androidNotificationDetail = AndroidNotificationDetails(
         ("channel Id"), "channel Name",
         channelDescription: "Demo channel description",
         priority: Priority.high,
-        importance: Importance.max);
-
+        importance: Importance.max,
+        color: ProductColor.redAccent,
+        styleInformation: const DefaultStyleInformation(true, true)
+    );
+    // var si=DefaultStyleInformation();
+    // var si1=DefaultStyleInformation();
+    // si1.
+    // var si2=InboxStyleInformation();
+    // si1.
+print("title $title" );
     var iosNotificationDetail = const DarwinNotificationDetails();
     var notificationDetail = NotificationDetails(
         android: androidNotificationDetail, iOS: iosNotificationDetail);
-
     await _flp.show(0, title, msg, notificationDetail,
         payload: "Burayi ANLAMADIM ???");
   }
