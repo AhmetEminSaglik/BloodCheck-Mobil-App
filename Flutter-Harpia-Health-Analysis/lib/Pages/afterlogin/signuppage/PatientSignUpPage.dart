8b938e128d560ca438cc41757929e857837ed9c9
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2023-09-03 14:51:31 +0300

    signup doctor and patient page is refactored.

--- Flutter-Harpia-Health-Analysis/lib/Pages/afterlogin/signuppage/PatientSignUpPage.dart
+++ Flutter-Harpia-Health-Analysis/lib/Pages/afterlogin/signuppage/PatientSignUpPage.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_harpia_health_analysis/httprequest/HttpRequestPatient.dart';
 import 'package:flutter_harpia_health_analysis/model/userrole/EnumUserRole.dart';
 import 'package:flutter_harpia_health_analysis/util/CustomAlertDialog.dart';
+import '../../../Product/FormCustomInput.dart';
 import '../../../business/factory/UserFactory.dart';
 import '../../../core/ResponsiveDesign.dart';
 import '../../../httprequest/HttpRequestDoctor.dart';
@@ -66,23 +66,30 @@ class _PatientSignUpPageState extends State<PatientSignUpPage> {
                   children: [
                     diabeticTypeDropDownMenu,
                     doctorDropDownMenu,
-                    _FormInputTextField(
-                        hintText: "Username",
-                        controller: tfUsername,
-                        obscure: false),
-                    _FormInputTextField(
-                        hintText: "Password",
-                        controller: tfPassword,
-                        obscure: true),
-                    _FormInputTextField(
-                        hintText: "Name", controller: tfName, obscure: false),
-                    _FormInputTextField(
-                        hintText: "Lastname",
-                        controller: tfLastname,
-                        obscure: false),
-                    // const _DiabeticTypeDropdownMenuButton(),
-                    /*_DiabeticTypeDropdownMenuButton(
-                        selectedDiabeticTypeValue: selectedDiabeticTypeValue),*/
+                    FormInputTextField(
+                      hintText: "Username",
+                      controller: tfUsername,
+                      obscure: false,
+                      compulsoryArea: false,
+                    ),
+                    FormInputTextField(
+                      hintText: "Password",
+                      controller: tfPassword,
+                      obscure: true,
+                      compulsoryArea: false,
+                    ),
+                    FormInputTextField(
+                      hintText: "Name",
+                      controller: tfName,
+                      obscure: false,
+                      compulsoryArea: false,
+                    ),
+                    FormInputTextField(
+                      hintText: "Lastname",
+                      controller: tfLastname,
+                      obscure: false,
+                      compulsoryArea: false,
+                    ),
                     _SignUpButton(
                       formKey: _formKey,
                       tfUsername: tfUsername,
@@ -318,28 +325,6 @@ class _DiabeticTypeDropdownMenuButtonState
   }
 }
 
-class _FormInputTextField extends StatelessWidget {
-  final TextEditingController controller;
-  final String hintText;
-  final bool obscure;
-
-  const _FormInputTextField(
-      {required this.controller,
-      required this.hintText,
-      required this.obscure});
-
-  @override
-  Widget build(BuildContext context) {
-    return _InputTextFieldPadding(
-      widget: _InputTextFormField(
-        hint: hintText,
-        textEditController: controller,
-        obscureText: obscure,
-      ),
-    );
-  }
-}
-
 class _SignUpButton extends StatelessWidget {
   final TextEditingController tfUsername, tfPassword, tfName, tfLastname;
   final _DiabeticTypeDropdownMenuButton selectedDiabeticType;
@@ -373,11 +358,6 @@ class _SignUpButton extends StatelessWidget {
                 style: TextStyle(
                     fontSize: ResponsiveDesign.getScreenWidth() / 20))));
   }
-
-  // void resetFormData() {
-  //   _formKey.currentState!.reset();
-  // }
-  //
 
   void resetPageInputs(
       List<TextEditingController> list,
@@ -461,82 +441,4 @@ class _SignUpButton extends StatelessWidget {
             msg: msg,
             roleId: EnumUserRole.PATIENT.roleId));
   }
-}
-
-class _InputTextFieldPadding extends StatelessWidget {
-  final StatelessWidget widget;
-
-  const _InputTextFieldPadding({super.key, required this.widget});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(
-          left: ResponsiveDesign.getScreenWidth() / 30,
-          right: ResponsiveDesign.getScreenWidth() / 30,
-          top: ResponsiveDesign.getScreenWidth() / 30,
-          bottom: ResponsiveDesign.getScreenWidth() / 25),
-      child: widget,
-    );
-  }
-}
-
-class _InputTextFormField extends StatelessWidget {
-  final String hint;
-  final TextEditingController textEditController;
-  final bool obscureText;
-
-  const _InputTextFormField(
-      {required this.hint,
-      required this.textEditController,
-      required this.obscureText});
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      // inputFormatters: [FilteringTextInputFormatter.allow(RegExp("[a-zA-Z]"))],
-      inputFormatters: <TextInputFormatter>[
-        FilteringTextInputFormatter.allow(RegExp('[a-z A-Z 0-9]'))
-      ],
-      maxLength: _TextFieldInputLength.max,
-      controller: textEditController,
-      obscureText: obscureText,
-      validator: (data) {
-        if (data!.isEmpty) {
-          return "Please enter $hint";
-        }
-        if (data.length < _TextFieldInputLength.min) {
-          return "Please enter ${_TextFieldInputLength.min} or more  character";
-        }
-        if (data.length > _TextFieldInputLength.max) {
-          return "Please enter ${_TextFieldInputLength.max} or less  character";
-        }
-        // return null;
-      },
-      decoration: InputDecoration(
-          labelText: hint,
-          labelStyle: TextStyle(
-              fontSize: ResponsiveDesign.getScreenWidth() / 23,
-              color: ProductColor.black,
-              fontWeight: FontWeight.bold),
-          hintText: hint,
-          hintStyle:
-              TextStyle(fontSize: ResponsiveDesign.getScreenWidth() / 20),
-          focusedBorder: OutlineInputBorder(
-              borderRadius: const BorderRadius.all(Radius.circular(10.0)),
-              borderSide: BorderSide(color: ProductColor.darkBlue)),
-          filled: true,
-          fillColor: ProductColor.white,
-          border: const OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(10.0)))),
-      style: TextStyle(
-          fontSize: ResponsiveDesign.getScreenWidth() / 22,
-          color: ProductColor.darkBlue),
-    );
-  }
-}
-
-class _TextFieldInputLength {
-  static int min = 3;
-  static int max = 10;
 }
