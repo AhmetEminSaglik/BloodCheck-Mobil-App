5439104a2e486f80583d27ab795074aa3c2fcd7a
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2023-09-04 02:12:57 +0300

    update Admin-Doctor-Patient Video is created and added to file

--- Flutter-Harpia-Health-Analysis/lib/util/ListViewUtilItemColor.dart
+++ Flutter-Harpia-Health-Analysis/lib/util/ListViewUtilItemColor.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_harpia_health_analysis/util/ProductColor.dart';
 
-class ListViewUtilItemColor{
+class ListViewUtilItemColor {
   static Color getBackgroundColor({required int index}) {
-    return index % 2 == 0 ? Colors.cyanAccent : Colors.tealAccent;
+    return index % 2 == 0 ? ProductColor.white : ProductColor.darkWhite;
   }
 }
