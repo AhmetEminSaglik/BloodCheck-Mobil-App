b5f52ef46cae7b735c8e59e859816b6308aed7fa
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2023-09-03 20:33:09 +0300

    Admin update profile REST API is created

--- Harpia-Health-Analysis-WS/src/main/java/com/harpia/HarpiaHealthAnalysisWS/controller/user/AdminController.java
+++ Harpia-Health-Analysis-WS/src/main/java/com/harpia/HarpiaHealthAnalysisWS/controller/user/AdminController.java
@@ -5,6 +5,7 @@ import com.harpia.HarpiaHealthAnalysisWS.business.concretes.signup.SignupUser;
 import com.harpia.HarpiaHealthAnalysisWS.controller.user.role.UserRoleController;
 import com.harpia.HarpiaHealthAnalysisWS.model.enums.EnumUserRole;
 import com.harpia.HarpiaHealthAnalysisWS.model.users.Admin;
+import com.harpia.HarpiaHealthAnalysisWS.model.users.Doctor;
 import com.harpia.HarpiaHealthAnalysisWS.model.users.User;
 import com.harpia.HarpiaHealthAnalysisWS.utility.CustomLog;
 import com.harpia.HarpiaHealthAnalysisWS.utility.result.DataResult;
@@ -22,19 +23,28 @@ import org.springframework.web.bind.annotation.*;
 public class AdminController {
     private static CustomLog log = new CustomLog(AdminController.class);
     @Autowired
-    private UserService service;
+    private UserService userService;
 
     @PostMapping("/save")
     public ResponseEntity<DataResult<User>> saveAdmin(@RequestBody Admin inputAdmin) {
         inputAdmin.setRoleId(EnumUserRole.ADMIN.getId());
-        SignupUser signupUser = new SignupUser(service);
+        SignupUser signupUser = new SignupUser(userService);
         DataResult<User> dataResult = signupUser.signup(inputAdmin);
         return ResponseEntity.status(HttpStatus.CREATED).body(dataResult);
     }
 
     @GetMapping("/{id}")
     public DataResult<Admin> findById(@PathVariable long id) {
-        Admin admin = (Admin) service.findById(id);
+        Admin admin = (Admin) userService.findById(id);
         return new SuccessDataResult<>(admin, "Admin retrived Succesfully");
     }
+
+    @PutMapping()
+    public ResponseEntity<DataResult<Doctor>> updateAdmin(@RequestBody Admin newAdmin) {
+        newAdmin = (Admin) userService.save(newAdmin);
+        String msg = "Doctor is updated";
+        DataResult<Doctor> result = new SuccessDataResult(newAdmin, msg);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
 }
