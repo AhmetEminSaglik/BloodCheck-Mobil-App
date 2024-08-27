bb433e8a359555cef6f3a2a0f30f54bdc6986d36
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2024-08-27 14:15:20 +0300

    Added listview to pickup Patient in Addbloodresult page

--- frontend/src/components/BloodResult/AddBloodResult.jsx
+++ frontend/src/components/BloodResult/AddBloodResult.jsx
@@ -1,13 +1,11 @@
-// AddBloodResult.jsx
-import React, { useState } from 'react';
-import axios from 'axios'; 
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './AddBloodResult.css';
 import ApiUrl from '../ApiURL/ApiURL';
 
 const AddBloodResult = () => {
-    const [patientId, setPatientId] = useState('');
-    const [patientName, setPatientName] = useState('');
-    const [isPatientFound, setIsPatientFound] = useState(null);
+    const [patients, setPatients] = useState([]); // Hasta listesini tutacak
+    const [selectedPatientId, setSelectedPatientId] = useState(null); // Seçilen hasta ID'si
     const [bloodSugar, setBloodSugar] = useState(0);
     const [bloodPressure, setBloodPressure] = useState(0);
     const [calcium, setCalcium] = useState(0);
@@ -15,31 +13,27 @@ const AddBloodResult = () => {
     const [errors, setErrors] = useState({});
     const [responseData, setResponseData] = useState('');
 
-    const fetchPatientName = async () => {
-
-        try {
-            var url=`${ApiUrl.getPatientsUrl()}/${patientId}`;
-            const response = await axios.get(url);
-            
-            if (response.data.success) {
-                const data = response.data.data;
-                setPatientName(`${data.name} ${data.lastname}`);
-                setIsPatientFound(true);
+    // Hastaları çekmek için bir API çağrısı
+    useEffect(() => {
+        const fetchPatients = async () => {
+            try {
+                const response = await axios.get(ApiUrl.getPatientsUrl());
+                if (response.data.success) {
+                    setPatients(response.data.data); // Hastaları kaydet
+                }
+            } catch (error) {
+                console.error('Fetch patients error:', error);
             }
-        } catch (error) {
-            setIsPatientFound(false);
-            console.error('Fetch error:', error);
-            return null; 
-        }
-    };
+        };
+
+        fetchPatients();
+    }, []);
 
     const handleSave = async () => {
-
-
         const newErrors = {};
 
-        if (patientId <= 0) {
-            newErrors.patientId = 'The Patient ID value must be at least 1.';
+        if (!selectedPatientId) {
+            newErrors.patient = 'You should select an patient.';
         }
 
         if (bloodSugar < 0 || bloodSugar > 200) {
@@ -60,21 +54,11 @@ const AddBloodResult = () => {
 
         setErrors(newErrors);
 
-        if (Object.keys(newErrors).length === 0 && isPatientFound) {
-            // Verileri kaydetmek için gerekli işlemleri burada yapın
-            console.log({
-                patientId,
-                patientName,
-                bloodSugar,
-                bloodPressure,
-                calcium,
-                magnesium,
-            });
-
+        if (Object.keys(newErrors).length === 0) {
             try {
-                var url=ApiUrl.getBloodResultsUrl();
-                const response = await axios.post(url,{
-                    patientId:patientId,
+                var url = ApiUrl.getBloodResultsUrl();
+                const response = await axios.post(url, {
+                    patientId: selectedPatientId,
                     bloodSugar: bloodSugar,
                     bloodPressure: bloodPressure,
                     calcium: calcium,
@@ -86,91 +70,84 @@ const AddBloodResult = () => {
             } catch (error) {
                 console.error('Axios POST error:', error);
             }
-        } else {
-            alert("Please type available values");
         }
-    };
-
-    const isFormValid = Object.keys(errors).length === 0 && patientId > 0 && isPatientFound === true;
-
-    const handleInputFocus = () => {
-        setResponseData(null); 
+      
     };
 
     return (
         <div className="add-blood-result">
             <h2>Add Blood Result</h2>
             <div className="input-group">
-                {/* <label>Patient Id (7 or bigger)</label> */}
-                <label>Patient Id </label>
-                <input
-                    type="number"
-                    min="1"
-                    value={patientId}
-                    onChange={(e) => setPatientId(e.target.value)}
-                    onFocus={handleInputFocus}
-                    onBlur={fetchPatientName} 
-                />
-                <label style={{ color: isPatientFound === true ? 'green' : isPatientFound === false ? 'red' : 'inherit' }}>
-                    {isPatientFound === true ? `Found Patient: ${patientName}` : isPatientFound === false ? 'Patient is not Found' : ''}
-                </label>
-                <br />
-                {errors.patientId && <span className="error">{errors.patientId}</span>}
+                <div className="input-label">
+                    <label >Patient </label>
+                </div>
+                <select
+                    className="input-group-listview"
+                    value={selectedPatientId || ''}
+                    onChange={(e) => setSelectedPatientId(e.target.value)}
+                >
+                    <option value="">Select Patient</option>
+                    {patients.map(patient => (
+                        <option key={patient.id} value={patient.id}>
+                            {patient.name} {patient.lastname}
+                        </option>
+                    ))}
+                </select>
+                {errors.patient && <span className="error">{errors.patient}</span>}
             </div>
             <div className="input-group">
-                <label>Blood Sugar (0-200)</label>
+                <div className="input-label">
+                    <label >Blood Sugar (0-200) </label>
+                </div>
                 <input
                     type="number"
                     value={bloodSugar}
                     onChange={(e) => setBloodSugar(e.target.value)}
-                    onFocus={handleInputFocus}
-                    min="0"  
-                    max="200"  
+                    min="0"
+                    max="200"
                 />
                 {errors.bloodSugar && <span className="error">{errors.bloodSugar}</span>}
             </div>
             <div className="input-group">
-                <label>Blood Pressure (0-200)</label>
+                <div className="input-label">
+                    <label >Blood Pressure (0-200) </label>
+                </div>
                 <input
                     type="number"
                     value={bloodPressure}
                     onChange={(e) => setBloodPressure(e.target.value)}
-                    onFocus={handleInputFocus}
-                    min="0"  
-                    max="200"  
+                    min="0"
+                    max="200"
                 />
-                {errors.bloodPressure && (
-                    <span className="error">{errors.bloodPressure}</span>
-                )}
+                {errors.bloodPressure && <span className="error">{errors.bloodPressure}</span>}
             </div>
             <div className="input-group">
-                <label>Calcium (0-200)</label>
+                <div className="input-label">
+                    <label >Calcium (0-200) </label>
+                </div>
                 <input
                     type="number"
                     value={calcium}
                     onChange={(e) => setCalcium(e.target.value)}
-                    onFocus={handleInputFocus}
-                    min="0" 
+                    min="0"
                     max="200"
                 />
                 {errors.calcium && <span className="error">{errors.calcium}</span>}
             </div>
             <div className="input-group">
-                <label>Magnesium (0-200)</label>
+                <div className="input-label">
+                    <label >Magnesium (0-200) </label>
+                </div>
                 <input
                     type="number"
                     value={magnesium}
                     onChange={(e) => setMagnesium(e.target.value)}
-                    onFocus={handleInputFocus}
-                    min="0" 
-                    max="200" 
+                    min="0"
+                    max="200"
                 />
                 {errors.magnesium && <span className="error">{errors.magnesium}</span>}
             </div>
-            <button
-                onClick={handleSave}>
-                Save
-            </button>
+            <button onClick={handleSave}>Save</button>
             <label style={{ color: responseData && responseData.success === true ? 'green' : 'red' }}>
                 {responseData && <p>{responseData.message}</p>}
             </label>
