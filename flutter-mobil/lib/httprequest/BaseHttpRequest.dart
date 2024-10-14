dd68930d513f2ac96028947045abacae29a00dce
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2024-10-15 00:08:38 +0300

    data manipulator: Reset data, go web Url buttons are added

--- flutter-mobil/lib/httprequest/BaseHttpRequest.dart
+++ flutter-mobil/lib/httprequest/BaseHttpRequest.dart
@@ -1,10 +1,10 @@
 class BaseHttpRequestConfig {
   // static const String _localhost = "http://10.0.2.2:";
   static const String _port = "8080";
-  static const String _localhost = "http://192.168.1.56:$_port";
+  // static const String _localhost = "http://192.168.1.56:$_port";
   // static const String _remotehost = "https://ws-bloodcheck.onrender.com";
-  static const String _remotehost = "http://remote-Host-IP:$_port";
-  static const String _host =_remotehost;
+  static const String remoteIp = "http://remote-ip:";
+  static const String _host ="$remoteIp$_port";
   // static const String _host =_localhost;
   static const String _apiVersion = "/api/1.0";
   static const String _baseUrl = _host + _apiVersion;
