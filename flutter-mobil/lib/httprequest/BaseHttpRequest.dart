46f6c5679bb9c4816e07df85d222bd1519113d27
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2025-01-02 22:38:06 +0300

    Properties are updated

--- flutter-mobil/lib/httprequest/BaseHttpRequest.dart
+++ flutter-mobil/lib/httprequest/BaseHttpRequest.dart
@@ -1,12 +1,11 @@
 class BaseHttpRequestConfig {
   // static const String _localhost = "http://10.0.2.2:";
-  static const String _port = "8080";
-  // static const String _localhost = "http://192.168.1.56:$_port";
-  // static const String _remotehost = "https://ws-bloodcheck.onrender.com";
-  static const String remoteIp = "http://remote-ip:";
+  static const String _port = "8081";
+  static const String remoteIp = "http://remoteIp:";
   static const String _host ="$remoteIp$_port";
   // static const String _host =_localhost;
-  static const String _apiVersion = "/api/1.0";
+  // static const String _apiVersion = "/api/1.0";
+  static const String _apiVersion = "";
   static const String _baseUrl = _host + _apiVersion;
 
   static String get baseUrl => _baseUrl;
