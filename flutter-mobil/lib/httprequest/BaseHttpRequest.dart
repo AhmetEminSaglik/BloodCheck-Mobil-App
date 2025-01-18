b881963a5c9b1bb787a344ee32c20ee7b5a6f725
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2025-01-19 01:01:51 +0300

    - url is updated

--- flutter-mobil/lib/httprequest/BaseHttpRequest.dart
+++ flutter-mobil/lib/httprequest/BaseHttpRequest.dart
@@ -1,8 +1,10 @@
 class BaseHttpRequestConfig {
   // static const String _localhost = "http://10.0.2.2:";
-  static const String _port = "8081";
-  static const String remoteIp = "http://remoteIp:";
-  static const String _host ="$remoteIp$_port";
+  // static const String _port = "8081";
+  // static const String remoteIp = "http://remoteIp:";
+  static const String remoteIp = "https://bloodcheck.ahmeteminsaglik.com";
+  // static const String _host ="$remoteIp:$_port";
+  static const String _host ="$remoteIp";
   // static const String _host =_localhost;
   // static const String _apiVersion = "/api/1.0";
   static const String _apiVersion = "";
--- frontend/src/components/ApiURL/ApiURL.jsx
+++ frontend/src/components/ApiURL/ApiURL.jsx
@@ -1,7 +1,10 @@
 class ApiUrl {
-    static localhost= "http://localhost:8081/";
+    // static localhost= "http://localhost:8081/";
     // static remotehost = "http://172.17.0.1:8081/";
-    static remotehost = "http://remoteIp:8081/";
+    // static remotehost = "http://172.17.0.1:8081/";
+    // static remotehost = "http://ws-bloodcheck:8081/";
+    // static remotehost = "http://my-network:8081/";
+    static remotehost = "http://178.18.241.162:8081/";
     // static BASE_URL=this.localhost;
     static BASE_URL=this.remotehost;
 
