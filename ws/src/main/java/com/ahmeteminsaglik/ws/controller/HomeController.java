dcc0c7e4715d9449f85b68ab86b7b5a4fc3b6bb1
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2025-02-26 00:06:39 +0300

    - HomeController is updated: <ul> and text are updated.

--- ws/src/main/java/com/ahmeteminsaglik/ws/controller/HomeController.java
+++ ws/src/main/java/com/ahmeteminsaglik/ws/controller/HomeController.java
@@ -2,7 +2,7 @@ package com.ahmeteminsaglik.ws.controller;
 
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -17,24 +17,23 @@ public class HomeController {
     @ResponseBody
     public String home() {
 
-        // Endpoint ve rolleri depolamak için bir Map
-        Map<String, List<String>> endpoints = new HashMap<>();
+        // Endpoint ve rolleri depolamak için bir LinkedHashMap
+        Map<String, List<String>> endpoints = new LinkedHashMap<>();
 
         // Gerçek endpointler ve roller
+        endpoints.put(baseUrl + "/", List.of("Anyone"));
         endpoints.put(baseUrl + "/login", List.of("Anyone(Post Request)"));
 
-        endpoints.put(baseUrl + "/patients", List.of("Patient", "Doctor", "Admin"));
+        endpoints.put(baseUrl + "/patients", List.of("Patient"));
         endpoints.put(baseUrl + "/patients/{patientId}", List.of("Patient"));
         endpoints.put(baseUrl + "/patients/{patientId}/doctors", List.of("Patient"));
 
-        endpoints.put(baseUrl + "/doctors", List.of("Doctor", "Admin"));
-        endpoints.put(baseUrl + "/doctors/{doctorId}", List.of("Doctor", "Admin"));
-        endpoints.put(baseUrl + "/doctors/{doctorId}/patients", List.of("Doctor", "Admin"));
+        endpoints.put(baseUrl + "/doctors", List.of("Doctor"));
+        endpoints.put(baseUrl + "/doctors/{doctorId}", List.of("Doctor"));
+        endpoints.put(baseUrl + "/doctors/{doctorId}/patients", List.of("Doctor"));
 
         endpoints.put(baseUrl + "/timers", List.of("Patient"));
         endpoints.put(baseUrl + "/timers/patients/{id}", List.of("Patient"));
-
-        endpoints.put(baseUrl + "/patients/{id}/doctors", List.of("Patient"));
 
         endpoints.put(baseUrl + "/bloodresults", List.of("Patient"));
         endpoints.put(baseUrl + "/bloodresults/patient/{patientId}", List.of("Patient"));
@@ -55,7 +54,13 @@ public class HomeController {
                         <style>
                             body { font-family: Arial, sans-serif; text-align: center; padding: 20px; }
                             h1 { color: #d32f2f; }
-                            ul { list-style-type: none; padding: 0; }
+                            ul {
+                            list-style-type: none;
+                            }
+                            ul.show-points {
+                                list-style-type: disc;
+                            }
+                                
                             li { margin: 10px 0; }
                             a { text-decoration: none; color: #1565c0; font-weight: bold; }
                             .role { font-size: 0.9em; color: gray; }
@@ -72,15 +77,24 @@ public class HomeController {
                         </style>
                     </head>
                     <body>
-                        <h1>Welcome to Bloodcheck API</h1>
+                        <h1>Welcome to the Bloodcheck API</h1>
                         <p>Explore the available endpoints below:</p>
                         <div class="notes-container">
                             <div class="notes">
                                 <h3 style="color:red">Important Notes</h3>
-                                <p>- Only GET requests are listed except for the login endpoint, which only accepts POST requests.</p>
-                                <p>- Patients can only access endpoints with Patient role permissions.</p>
-                                <p>- Doctors can request endpoints with both Patient and Doctor role permissions.</p>
-                                <p>- Admins can access all endpoints.</p>
+                                <ul class="show-points">
+                                <li> Only GET requests are listed, except for the login endpoint, which accepts only POST requests.</li>
+                                <li> You can send POST Request to Login url. Then you can use the token in postman for <span style="font-weight:bold">Bearer Token </span></li>
+                                <li> Login Credentials:
+                                    <ul class="show-points"> 
+                                        <li>5 doctors, 20 patients are registered. A username can be selected such as doctor1 or patient15</li>
+                                        <li>Example Login Request (JSON format): { "username":"patient15","password":"pass" }</li>
+                                     </ul> 
+                                </li>
+                                <li> Patients can only access endpoints with Patient role permissions.</li>
+                                <li> Doctors can request endpoints with both Patient and Doctor role permissions.</li>
+                                <li> Admins can access all endpoints.</li>
+                                </ul>
                             </div>
                         </div>
                         <ul>
